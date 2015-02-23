dd52cb51fd8ccdbcd3e003e5bcc588214c3daccc
Pete Olsen <hepaestus@gmail.com>
2015-02-23 13:02:35 -0500

    Work on spells and possible filtering of skills

--- app.js
+++ app.js
@@ -185,7 +185,7 @@
       { level:'High', type:'Artisan', sub_type:'Origami', trait:'awareness', ring:'air', rank:0, rank_s:0, roll:'', emphasis:null, description:'Pg. 135 Core Book'},
       { level:'High', type:'Artisan', sub_type:'Bonsai', trait:'awareness', ring:'air', rank:0, rank_s:0, roll:'', emphasis:null, description:'Pg. 135 Core Book'},
       { level:'High', type:'Artisan', sub_type:'Gardening', trait:'awareness', ring:'air', rank:0, rank_s:0, roll:'', emphasis:null, description:'Pg. 135 Core Book'},
-      { level:'High', type:'Artisan', sub_type:'Ikebana', trait:'awareness', ring:'air', rank:0, rank_s:0, roll:'', emphasis:null, description:'(flower aranging) Pg. 135 Core Book'},
+      { id:6, level:'High', type:'Artisan', sub_type:'Ikebana', trait:'awareness', ring:'air', rank:0, rank_s:0, roll:'', emphasis:null, description:'(flower aranging) Pg. 135 Core Book'},
       { level:'High', type:'Artisan', sub_type:'Painting', trait:'awareness', ring:'air', rank:0, rank_s:0, roll:'', emphasis:null, description:'Pg. 135 Core Book'},
       { level:'High', type:'Artisan', sub_type:'Poetry', trait:'awareness', ring:'air', rank:0, rank_s:0, roll:'', emphasis:null, description:'Pg. 135 Core Book'},
       { level:'High', type:'Artisan', sub_type:'Sculpture', trait:'awareness', ring:'air', rank:0, rank_s:0, roll:'', emphasis:null, description:'Pg. 135 Core Book'},
@@ -261,6 +261,7 @@
     ];
 
     $scope.showSkillList = false;
+    $scope.skillSearchText = "";
     $scope.toggleShowSkillsList = function() {
         $scope.showSkillsList = !$scope.showSkillsList;
     };
@@ -307,9 +308,11 @@
       if ( $scope.character.skills[index].rank < $scope.character.skills[index].rank_s ) {
         var diff = $scope.character.skills[index].rank_s - $scope.character.skills[index].rank;
         $scope.character.experience_points += diff;
+        $scope.character.insight_rank -= diff;
       } else if ( $scope.character.skills[index].rank > $scope.character.skills[index].rank_s ) {
         var diff = $scope.character.skills[index].rank - $scope.character.skills[index].rank_s;
 	    $scope.character.experience_points -= diff;
+        $scope.character.insight_rank += diff;
       }
       $scope.character.skills[index].rank_s = $scope.character.skills[index].rank;
       $scope.updateSkills($scope.character.skills[index].trait, $scope.character.skills[index].ring);
@@ -319,59 +322,21 @@
 
   app.controller('SpellsController', function($scope) {
 
-    function Spell(name,level,ring,range,area_of_affect,duration,raises,description) {
-      this.name = name;
-      this.level = level;
-      this.ring = ring;
-      this.range = range;
-      this.area_of_affect = area_of_affect;      
-      this.duration = duration;
-      this.raises = raises;
-      this.description = description;
-    }
+    $scope.spellsMasterList = [
+      { id:0, name:'Sense', ring:'all', level: '1', range:'Personal', area_of_affect:'50\' radius from the caster', duration:'Instantaneous', raises: 'Range(+10\')', description:'This spell can be cast in any of the four standard elements. It allows for the caster to sense the presense, quantity, and rough location of the elemental spirits (not evil spirits known as <i>kansen</i> of that element within the range of the spell. This is most frequently applied when looking for spirits with which to Commune (See Commune), but can also can be useful as a crude basic location device. For example, a caster lost in the wilderness could cast Snese(Water) in hopes of locating the sourceof drinking water.' },
+      { id:1, name:'Summon', ring:'all', level: '1', range:'30\'', area_of_affect:'1 cubic foot of summoned matterial', duration:'Permanent', raises: 'Range(+10\'), Quantity(+1 cubic foot), Composition of Material(1-4 raises as outlined below)', description:'This spell can be cast in any of the tour standard elements. It allows the caster to summon a modest quantity (one cubic foot) of the chosen element. The summoned matter appears (usually in a rough ball shape] in any open space within the spell\'s range. This cannot place the summoned material inside another physical object or living creature. The summoned element will behave in a normal and mundane matter — earth falls to the ground, water soaks anything it lands on, air blows away, and ﬁre winks out unless there is something present for it to burn. In general it is impossible to use this spell effectively in combat, although clever shugenja may find a few modest combat uses. such as using Summon [Fire] to ignite a foe soaked in cooking oil. More commonly, the Spell\’s value is in simpler functions. such as summoning Water while in a desert, or summoning Fire to light a campfire without flint and tinder. Raises may be used with this spell to summon a more speciﬁc type of the appropriate element, such as wood or iron with Earth, or tea with Water. The GM should choose how many Raises (generally anywhere from 1 to 4) this requires. However, these Raises cannot be used to create rare or precious materials (such as gold) or spiritually powerful substances (such as jade or crystal).' },
+      { id:2, name:'Commune', ring:'all', level: '1', range:'20\'', area_of_affect:'self', duration:'Concentration', raises: 'See Desc.', description:'This spell can be cast in any element save Void. It allows the caster to speak with one of the local elemental kami, asking it a few questions, which is will answer honestly to the best of it\'s ability. ...' },
+      { id:3, name:'Blessed Wind', ring:'air', level:'1', range:'Personal', area_of_affect:'10\' radius around the caster', duration:'Concentration', raises:'Special(you may target another spell with this spell with 3 raises)', description:'Pg 167 Core Book' },
+    ];
 
-    $scope.spellsMasterList = {
-      'universal': {
-        '1': [
-          { 'name':'Sense', 'ring':'all', 'level': '1', 'range':'Personal', 'area_of_affect':'50\' radius from the caster', 'duration':'Instantaneous', 'raises': 'Range(+10\')', 'description':"This spell can be cast in any of the four standard elements. It allows for the caster to sense the presense, quantity, and rough location of the elemental spirits (not evil spirits known as <i>kansen</i> of that element within the range of the spell. This is most frequently applied when looking for spirits with which to Commune (See Commune), but can also can be useful as a crude basic location device. For example, a caster lost in the wilderness could cast Snese(Water) in hopes of locating the sourceof drinking water." },
-          { 'name':'Summon', 'ring':'all', 'level': '1', 'range':'30\'', 'area_of_affect':'1 cubic foot of summoned matterial', 'duration':'Permanent', 'raises': 'Range(+10\'), Quantity(+1 cubic foot), Composition of Material(1-4 raises as outlined below)', 'description':"s spell can be cast in any of the tour standard elements. It allows the caster to summon a modest quantity (one cubic foot) of the chosen element. The summoned matter appears (usually in a rough ball shape] in any open space within the spell\'s range. This cannot place the summoned material inside another physical object or living creature. The summoned element will behave in a normal and mundane matter — earth falls to the ground, water soaks anything it lands on, air blows away, and ﬁre winks out unless there is something present for it to burn. In general it is impossible to use this spell effectively in combat, although clever shugenja may find a few modest combat uses. such as using Summon [Fire] to ignite a foe soaked in cooking oil. More commonly, the Spell’s value is in simpler functions. such as summoning Water while in a desert, or summoning Fire to light a campfire without flint and tinder. Raises may be used with this spell to summon a more speciﬁc type of the appropriate element, such as wood or iron with Earth, or tea with Water. The GM should choose how many Raises (generally anywhere from 1 to 4) this requires. However, these Raises cannot be used to create rare or precious materials (such as gold) or spiritually powerful substances (such as jade or crystal)." },
-          { 'name':'Commune', 'ring':'all', 'level': '1', 'range':'20\'', 'area_of_affect':'self', 'duration':'Concentration', 'raises': 'See Desc.', 'description':"This spell can be cast in any element save Void. It allows the caster to speak with one of the local elemental kami, asking it a few questions, which is will answer honestly to the best of it's ability. ..." },
-         ],
-      },
-      'air': { 
-        '1': [
-          { 'name':'Blessed Wind', 'ring':'air', 'level':'1', 'range':'Personal', 'area_of_affect':'10\' radius around the caster', 'duration':'Concentration', 'raises': 'Special(you may target another spell with this spell with 3 raises)', 'description':"Pg 167 Core Book" },
-        ],
-        '2': [],
-        '3': [],
-        '4': [],
-        '5': [],
-        '6': [],
-      },
-      'water': {
-        '1': [],
-        '2': [],
-        '3': [],
-        '4': [],
-        '5': [],
-        '6': [],
-      },
-      'fire': {
-        '1': [],
-        '2': [],
-        '3': [],
-        '4': [],
-        '5': [],
-        '6': [],
-      },
-      'earth': {
-        '1': [],
-        '2': [],
-        '3': [],
-        '4': [],
-        '5': [],
-        '6': [],
-       }
+    $scope.showSpellsList = false;
+    $scope.toggleShowSpellsList = function() {
+      $scope.showSpellsList = !$scope.showSpellsList;
+    };
+
+    $scope.addSpell = function(index) {      
+      $scope.character.spells.push( $scope.spellsMasterList[index]) ;
+      $scope.toggleShowSpellsList();
     };
 
   });
